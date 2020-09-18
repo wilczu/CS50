fbd9810d62fc146ec71e0f1a7a56d99c987d65b7
Wilczu <kacpersoja1@gmail.com>
2020-09-18 15:01:51 +0000

    Add "create" page to add new wiki page and function to display and generate inputs.

--- project 1/encyclopedia/urls.py
+++ project 1/encyclopedia/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("wiki/<str:query>", views.wiki, name="wiki")
+    path("wiki/<str:query>", views.wiki, name="wiki"),
+    path("create", views.create_page, name="create")
 ]
--- project 1/encyclopedia/views.py
+++ project 1/encyclopedia/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django import forms
 
 from . import util
 
+class CreateForm(forms.Form):
+    title = forms.CharField(label="Title of your wiki page")
+    content = forms.CharField(label="Content of the page",
+        widget=forms.Textarea(attrs={'style' : 'height: 500px;'}))
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -21,3 +26,8 @@ def wiki(request, query):
         return render(request, "encyclopedia/error.html", {
             "error_message": (f"Query {query} was not found!")
         })
+
+def create_page(request):
+    return render(request, "encyclopedia/create.html", {
+        "create_form": CreateForm()
+    })

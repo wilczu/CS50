8c1da6fe35be1c3ddc23c7d14eb0ed102d92a219
Wilczu <kacpersoja1@gmail.com>
2020-09-19 22:03:16 +0000

    Add search website

--- project 1/encyclopedia/urls.py
+++ project 1/encyclopedia/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("wiki/<str:query>", views.wiki, name="wiki"),
-    path("create", views.create_page, name="create")
+    path("create", views.create_page, name="create"),
+    path("search", views.search, name="search")
 ]

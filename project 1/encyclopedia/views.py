495d5f1a65b85cc575b68f6760cb790b97ce0f38
Wilczu <kacpersoja1@gmail.com>
2020-09-23 14:42:43 +0000

    Implement markdown2 library

--- project 1/encyclopedia/views.py
+++ project 1/encyclopedia/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django import forms
 import random
+import markdown2
 from . import util
 
 class CreateForm(forms.Form):

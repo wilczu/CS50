b752bc0ad1c877fe1bdf27466ce97a42d83dd51c
Wilczu <kacpersoja1@gmail.com>
2020-09-23 11:00:56 +0000

    Add link to random page.

--- project 1/encyclopedia/urls.py
+++ project 1/encyclopedia/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("wiki/<str:query>", views.wiki, name="wiki"),
     path("create", views.create_page, name="create"),
     path("search", views.search, name="search"),
-    path("edit/<str:entry>", views.edit, name="edit")
+    path("edit/<str:entry>", views.edit, name="edit"),
+    path("random/", views.random_wiki, name="random")
 ]

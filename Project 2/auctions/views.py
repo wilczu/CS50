ee102c9f22bbd19a2587301d826c4d02e8fdbbfb
Wilczu <kacpersoja1@gmail.com>
2020-10-02 13:36:33 +0000

    Add create_listing page to add new listings with form.

--- Project 2/auctions/views.py
+++ Project 2/auctions/views.py
@@ -61,3 +61,7 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "auctions/register.html")
+
+
+def create_listing(request):
+    return render(request, "auctions/create_listing.html")

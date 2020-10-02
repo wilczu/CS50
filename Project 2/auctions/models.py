7e8eec3bc260e7a89e29b3ab8b68f152fb41d90c
Wilczu <kacpersoja1@gmail.com>
2020-10-02 18:25:40 +0000

    Add category table with fields and some content.

--- Project 2/auctions/models.py
+++ Project 2/auctions/models.py
@@ -4,3 +4,9 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+    def __str__(self):
+        return f"{self.id} : {self.name}"

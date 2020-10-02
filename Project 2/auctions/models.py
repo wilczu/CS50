5e8f045c20868518647555d95dec1a057efe1ffe
Wilczu <kacpersoja1@gmail.com>
2020-10-02 19:11:16 +0000

    Add Listings table.

--- Project 2/auctions/models.py
+++ Project 2/auctions/models.py
@@ -10,3 +10,13 @@ class Category(models.Model):
 
     def __str__(self):
         return f"{self.id} : {self.name}"
+
+class Listings(models.Model):
+    title = models.CharField(max_length=64)
+    description = models.CharField(max_length=256)
+    start_bid = models.DecimalField(max_digits=10000, decimal_places=2)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default="none", related_name="category_name")
+    image = models.CharField(max_length=128, default="none")
+
+    def __str__(self):
+        return f"{self.title} : {self.description} : {self.start_bid} : {self.category} : {self.image}"

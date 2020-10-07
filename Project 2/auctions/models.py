7ec0a72f46795df36b0a3d8f66d4326693851cc6
Wilczu <kacpersoja1@gmail.com>
2020-10-07 14:49:28 +0000

    Rename fields in watchlist table.

--- Project 2/auctions/models.py
+++ Project 2/auctions/models.py
@@ -29,8 +29,8 @@ class UserListing(models.Model):
         return f"{self.listing.id} : {self.user.id}"
 
 class watchlist(models.Model):
-    listingID = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name="watched")
-    userID = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watching")
+    listing = models.ForeignKey(Listings, on_delete=models.CASCADE, related_name="watched")
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="watching")
 
     def __str__(self):
         return f"{self.listingID.title} : {self.userID.username}"

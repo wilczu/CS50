fd4411679d486050a56cc3fc48cb20366ca85807
Wilczu <kacpersoja1@gmail.com>
2020-10-07 10:39:38 +0000

    rename create_listing link to create.

--- Project 2/auctions/urls.py
+++ Project 2/auctions/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("login", views.login_view, name="login"),
     path("logout", views.logout_view, name="logout"),
     path("register", views.register, name="register"),
-    path("listing", views.create_listing, name="create_listing")
+    path("create", views.create_listing, name="create_listing"),
 ]

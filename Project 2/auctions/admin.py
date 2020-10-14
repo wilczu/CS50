e4956d01345e2040e25b0e9c8bf5d5ecd5107860
Wilczu <kacpersoja1@gmail.com>
2020-10-14 22:21:49 +0000

    Adding appropriate admin panel configuration

--- Project 2/auctions/admin.py
+++ Project 2/auctions/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import User, Category, Listings, UserListing, watchlist, bidding
+from .models import User, Category, Listings, watchlist, bidding, comments
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Category)
 admin.site.register(Listings)
-admin.site.register(UserListing)
+admin.site.register(comments)
 admin.site.register(watchlist)
 admin.site.register(bidding)

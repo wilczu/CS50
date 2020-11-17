f2b0ae80b37c66ea74d61f2b19f8a136a96fefdf
Wilczu <kacpersoja1@gmail.com>
2020-11-17 20:40:17 +0000

    Add likes cell to the database and display numbers of likes for each post.

--- Project 4/network/models.py
+++ Project 4/network/models.py
@@ -6,9 +6,10 @@ class User(AbstractUser):
     pass
 
 class Posts(models.Model):
-    user_id = models.ForeignKey(User, on_delete= models.CASCADE, related_name="post_owner")
+    post_owner = models.ForeignKey(User, on_delete= models.CASCADE)
     post_content = models.CharField(max_length=2000)
+    post_likes = models.IntegerField(default=0)
     post_date = models.DateTimeField()
 
     def __str__(self):
-        return f"{self.post_content} {self.post_date}"
+        return f"{self.post_owner} {self.post_content} {self.post_date}"

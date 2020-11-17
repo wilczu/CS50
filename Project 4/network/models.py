915dc74b153c4763a6a7cd613019f2383e38560c
Wilczu <kacpersoja1@gmail.com>
2020-11-17 20:09:08 +0000

    Add Posts table and implement it to the admin panel.

--- Project 4/network/models.py
+++ Project 4/network/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+class Posts(models.Model):
+    user_id = models.ForeignKey(User, on_delete= models.CASCADE, related_name="post_owner")
+    post_content = models.CharField(max_length=2000)
+    post_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.post_content} {self.post_date}"

f14a543fa51b67ae66d936c64e4dda4b96b5e2c1
Wilczu <kacpersoja1@gmail.com>
2020-11-17 20:34:22 +0000

    Add displaying all posts on the main page

--- Project 4/network/views.py
+++ Project 4/network/views.py
@@ -4,11 +4,13 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .models import User
+from .models import User, Posts
 
 
 def index(request):
-    return render(request, "network/index.html")
+    return render(request, "network/index.html", {
+        "all_posts": Posts.objects.all()
+    })
 
 
 def login_view(request):
